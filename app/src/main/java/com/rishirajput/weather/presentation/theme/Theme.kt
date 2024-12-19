25cebc3e6d68d041db75542be62eda355c59e75c
rishirajput <rishi_raj@live.com>
2024-12-20 01:28:31 +0530

    remove unused imports in Theme.kt

--- app/src/main/java/com/rishirajput/weather/presentation/theme/Theme.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.rishirajput.weather.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
