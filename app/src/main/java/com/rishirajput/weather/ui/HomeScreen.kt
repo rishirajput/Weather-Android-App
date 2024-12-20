cc54f09f69d32ec99be12a042fa994060919c74f
rishirajput <rishi_raj@live.com>
2024-12-21 00:54:42 +0530

    create composable NoCitySelected

--- app/src/main/java/com/rishirajput/weather/ui/HomeScreen.kt
+++ app/src/main/java/com/rishirajput/weather/ui/HomeScreen.kt
@@ -1,26 +1,19 @@
 package com.rishirajput.weather.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rishirajput.weather.R
 import com.rishirajput.weather.presentation.theme.appBackgroundColor
-import com.rishirajput.weather.presentation.theme.textStyleNoCitySelected
-import com.rishirajput.weather.presentation.theme.textStyleSearchCityLabel
 
 @Composable
 fun HomeScreen(innerPadding: PaddingValues)  {
@@ -37,30 +30,11 @@ fun HomeScreen(innerPadding: PaddingValues)  {
             modifier = Modifier.padding(4.dp)
         )
         Spacer(modifier = Modifier.height(32.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = stringResource(R.string.no_city_selected),
-                    style = textStyleNoCitySelected,
-                    textAlign = TextAlign.Center
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = stringResource(R.string.please_search_for_a_city),
-                    style = textStyleSearchCityLabel,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
+        NoCitySelected()
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/rishirajput/weather/ui/NoCitySelected.kt
@@ -0,0 +1,51 @@
+package com.rishirajput.weather.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rishirajput.weather.R
+import com.rishirajput.weather.presentation.theme.textStyleNoCitySelected
+import com.rishirajput.weather.presentation.theme.textStyleSearchCityLabel
+
+@Composable
+fun NoCitySelected() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.no_city_selected),
+                style = textStyleNoCitySelected,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.please_search_for_a_city),
+                style = textStyleSearchCityLabel,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoCitySelectedPreview() {
+    NoCitySelected()
+}
