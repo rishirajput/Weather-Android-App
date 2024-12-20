e491daf5efe8959febd83f3184f62258362146a0
rishirajput <rishi_raj@live.com>
2024-12-21 00:01:43 +0530

    update HomeActivity.kt to display HomeScreen

--- app/src/main/java/com/rishirajput/weather/HomeActivity.kt
+++ app/src/main/java/com/rishirajput/weather/HomeActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rishirajput.weather.presentation.theme.WeatherTheme
+import com.rishirajput.weather.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen(innerPadding)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeEmptyStatePreview() {
     WeatherTheme {
-        Greeting("Android")
+        HomeScreen(innerPadding = PaddingValues(0.dp))
     }
 }
