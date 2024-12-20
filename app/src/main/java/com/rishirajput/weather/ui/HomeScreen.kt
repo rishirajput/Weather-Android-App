09d5ec98c65d6ad0bb772afb870e25c3c82101de
rishirajput <rishi_raj@live.com>
2024-12-21 00:01:21 +0530

    update HomeScreen.kt UI

--- app/src/main/java/com/rishirajput/weather/ui/HomeScreen.kt
+++ app/src/main/java/com/rishirajput/weather/ui/HomeScreen.kt
@@ -4,34 +4,38 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rishirajput.weather.R
+import com.rishirajput.weather.presentation.theme.appBackgroundColor
 import com.rishirajput.weather.presentation.theme.textStyleNoCitySelected
 import com.rishirajput.weather.presentation.theme.textStyleSearchCityLabel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(innerPadding: PaddingValues)  {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFFFFFFF)) // Light gray background for the screen
-            .padding(16.dp)
+            .background(appBackgroundColor) // Light gray background for the screen
+            .padding(innerPadding)
+            .padding(start = 20.dp, end = 20.dp)
     ) {
-        Spacer(modifier = Modifier.statusBarsPadding())
-        SearchBar(stringResource(id = R.string.search_location))
+        Spacer(modifier = Modifier.height(44.dp))
+        SearchBar(
+            placeholderText = stringResource(id = R.string.search_location),
+            modifier = Modifier.padding(4.dp)
+        )
         Spacer(modifier = Modifier.height(32.dp))
         Box(
             modifier = Modifier
@@ -61,5 +65,5 @@ fun HomeScreen() {
 @Preview(showBackground = true)
 @Composable
 fun HomeEmptyStatePreview() {
-    HomeScreen()
+    HomeScreen(innerPadding = PaddingValues(0.dp))
 }
