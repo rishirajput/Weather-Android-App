5f4b0f9e8bbcea945f729c68df3033285c685d9b
rishirajput <rishi_raj@live.com>
2024-12-21 22:01:21 +0530

    create WeatherDataSerializer.kt

---
+++ app/src/main/java/com/rishirajput/weather/data/serializer/WeatherDataSerializer.kt
@@ -0,0 +1,27 @@
+package com.rishirajput.weather.data.serializer
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.rishirajput.weather.domain.model.WeatherData
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+import java.io.OutputStream
+
+object WeatherDataSerializer : Serializer<WeatherData?> {
+    override val defaultValue: WeatherData? = null
+
+    override suspend fun readFrom(input: InputStream): WeatherData? {
+        return try {
+            val json = input.readBytes().decodeToString()
+            Json.decodeFromString<WeatherData>(json)
+        } catch (exception: Exception) {
+            throw CorruptionException("Cannot read JSON.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: WeatherData?, output: OutputStream) {
+        val json = Json.encodeToString(t)
+        output.write(json.encodeToByteArray())
+    }
+}
