78cafe2051232ecd722e6e98b54921dd91ac292c
rishirajput <rishi_raj@live.com>
2024-12-21 18:02:20 +0530

    add okhttp logging interceptor in AppModule.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
     implementation(libs.coil)
     implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
