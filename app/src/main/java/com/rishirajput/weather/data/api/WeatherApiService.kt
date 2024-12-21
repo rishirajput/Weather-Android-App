7fc5e093eed63986455a12c286a12063aa0573cb
rishirajput <rishi_raj@live.com>
2024-12-21 13:19:38 +0530

    optimize imports in WeatherApiService.kt

--- app/src/main/java/com/rishirajput/weather/data/api/WeatherApiService.kt
+++ app/src/main/java/com/rishirajput/weather/data/api/WeatherApiService.kt
@@ -1,10 +1,5 @@
 package com.rishirajput.weather.data.api
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
 
