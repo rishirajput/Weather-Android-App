16835b618da2e682d52cba7bba1bc91af3808d7f
rishirajput <rishi_raj@live.com>
2024-12-21 22:03:14 +0530

    make WeatherData Serializable

--- app/src/main/java/com/rishirajput/weather/domain/model/WeatherData.kt
+++ app/src/main/java/com/rishirajput/weather/domain/model/WeatherData.kt
@@ -1,5 +1,8 @@
 package com.rishirajput.weather.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class WeatherData(
     val locationName: String,
     val temperature: Double,
