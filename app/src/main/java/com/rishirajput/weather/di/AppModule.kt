a672d1ad46682f123bb3c7e2dfdf5c566b59517a
rishirajput <rishi_raj@live.com>
2024-12-21 18:01:46 +0530

    add okhttp logging interceptor in AppModule.kt

--- app/src/main/java/com/rishirajput/weather/di/AppModule.kt
+++ app/src/main/java/com/rishirajput/weather/di/AppModule.kt
@@ -7,6 +7,8 @@ import com.rishirajput.weather.domain.repository.WeatherRepository
 import com.rishirajput.weather.presentation.viewmodel.WeatherViewModel
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -14,8 +16,18 @@ import retrofit2.Retrofit
 fun provideRetrofit(): Retrofit {
     val contentType = "application/json".toMediaType()
     val json = Json { ignoreUnknownKeys = true }
+
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
     return Retrofit.Builder()
         .baseUrl("https://api.weatherapi.com/v1/")
+        .client(client)
         .addConverterFactory(json.asConverterFactory(contentType))
         .build()
 }
