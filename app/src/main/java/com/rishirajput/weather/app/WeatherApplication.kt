f172abb8251cbb8929d14f63a62b1a66aabaf329
rishirajput <rishi_raj@live.com>
2024-12-21 22:06:18 +0530

    provide androidContext in WeatherApplication.kt

--- app/src/main/java/com/rishirajput/weather/app/WeatherApplication.kt
+++ app/src/main/java/com/rishirajput/weather/app/WeatherApplication.kt
@@ -2,12 +2,14 @@ package com.rishirajput.weather.app
 
 import android.app.Application
 import com.rishirajput.weather.di.appModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class WeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(this@WeatherApplication)
             modules(appModule)
         }
     }
