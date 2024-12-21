ca43b0f1cf21340664286f6892fb983fc8fead61
rishirajput <rishi_raj@live.com>
2024-12-21 18:04:26 +0530

    update fieldName in WeatherData.kt

--- app/src/main/java/com/rishirajput/weather/domain/model/WeatherData.kt
+++ app/src/main/java/com/rishirajput/weather/domain/model/WeatherData.kt
@@ -1,10 +1,10 @@
-// WeatherData.kt
 package com.rishirajput.weather.domain.model
 
 data class WeatherData(
+    val locationName: String,
     val temperature: Double,
     val condition: String,
-    val iconUrl: String,
+    val icon: String,
     val humidity: Int,
     val uvIndex: Double,
     val feelsLike: Double
