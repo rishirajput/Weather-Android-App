7e6f68a903337f90673476cf71c7cfc8b00d790a
rishirajput <rishi_raj@live.com>
2024-12-21 18:09:28 +0530

    create LocationResultCard

--- app/src/main/java/com/rishirajput/weather/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/HomeScreen.kt
@@ -1,19 +1,26 @@
-// HomeScreen.kt
 package com.rishirajput.weather.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rishirajput.weather.R
-import com.rishirajput.weather.domain.model.WeatherData
 import com.rishirajput.weather.presentation.theme.appBackgroundColor
 import com.rishirajput.weather.presentation.ui.NoCitySelected
 import com.rishirajput.weather.presentation.ui.SearchBar
+import com.rishirajput.weather.presentation.ui.LocationResultCard
 import com.rishirajput.weather.presentation.viewmodel.WeatherViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -37,23 +44,13 @@ fun HomeScreen(innerPadding: PaddingValues) {
             onSearch = { query = it; viewModel.fetchWeatherData(it) }
         )
         Spacer(modifier = Modifier.height(32.dp))
-        weatherData?.let { data ->
-            WeatherCard(data)
-        } ?: NoCitySelected()
-    }
-}
-
-@Composable
-fun WeatherCard(weatherData: WeatherData) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Text(text = "Temperature: ${weatherData.temperature}°C")
-        Text(text = "Condition: ${weatherData.condition}")
-        Text(text = "Humidity: ${weatherData.humidity}%")
-        Text(text = "UV Index: ${weatherData.uvIndex}")
-        Text(text = "Feels Like: ${weatherData.feelsLike}°C")
+        if (weatherData.isNotEmpty()) {
+            weatherData.forEach { data ->
+                LocationResultCard(data)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        } else {
+            NoCitySelected()
+        }
     }
 }
---
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/LocationResultCard.kt
@@ -0,0 +1,72 @@
+package com.rishirajput.weather.presentation.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.rishirajput.weather.domain.model.WeatherData
+import com.rishirajput.weather.presentation.theme.textStyleCurrentTemperature
+import com.rishirajput.weather.presentation.theme.textStyleLoationName
+import highlightedBackGround
+import androidx.compose.ui.Alignment
+import com.rishirajput.weather.presentation.utils.updateImageSizeInUrl
+
+@Composable
+fun LocationResultCard(weatherData: WeatherData) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .highlightedBackGround()
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(
+                text = weatherData.locationName,
+                style = textStyleLoationName
+            )
+            Text(
+                text = "${weatherData.temperature}°",
+                style = textStyleCurrentTemperature,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+
+            )
+        }
+        val painter = rememberAsyncImagePainter(model = updateImageSizeInUrl("https:${weatherData.icon}", 128))
+        Image(
+            painter = painter,
+            contentDescription = "Weather Icon",
+            modifier = Modifier
+                .height(110.dp)
+                .width(110.dp)
+                .padding(bottom = 8.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLocationResult() {
+    val dummyWeatherData = WeatherData(
+        locationName = "Airport Village",
+        temperature = 27.3,
+        condition = "Patchy rain nearby",
+        icon = "//cdn.weatherapi.com/weather/64x64/night/176.png",
+        humidity = 76,
+        uvIndex = 0.1,
+        feelsLike = 30.4
+    )
+    LocationResultCard(weatherData = dummyWeatherData)
+}
