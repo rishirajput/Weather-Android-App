231342e33ad0fe19e7cbae35b44a25139926d940
rishirajput <rishi_raj@live.com>
2024-12-21 16:57:16 +0530

    create reusable HighlightedBackground.kt

---
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/HighlightedBackground.kt
@@ -0,0 +1,9 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.rishirajput.weather.presentation.theme.highLightColor
+
+fun Modifier.highlightedBackGround(color: Color = highLightColor) = this
+    .background(color, shape = RoundedCornerShape(16.dp))
