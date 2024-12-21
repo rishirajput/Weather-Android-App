54a11759be940ab5d3278c62696fc33f5996ecab
rishirajput <rishi_raj@live.com>
2024-12-21 18:06:03 +0530

    update WeatherRepository.kt to return List<WeatherData>

--- app/src/main/java/com/rishirajput/weather/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/rishirajput/weather/domain/repository/WeatherRepository.kt
@@ -3,5 +3,5 @@ package com.rishirajput.weather.domain.repository
 import com.rishirajput.weather.domain.model.WeatherData
 
 interface WeatherRepository {
-    suspend fun getWeatherData(query: String): WeatherData
+    suspend fun getWeatherData(query: String): List<WeatherData>
 }
