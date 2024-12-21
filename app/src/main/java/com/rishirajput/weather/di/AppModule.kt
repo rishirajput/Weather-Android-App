3e980aa4f42fd85816599ba0908b1da58884bdfc
rishirajput <rishi_raj@live.com>
2024-12-21 13:19:00 +0530

    provide retrofit in AppModule.kt

--- app/src/main/java/com/rishirajput/weather/di/AppModule.kt
+++ app/src/main/java/com/rishirajput/weather/di/AppModule.kt
@@ -1,12 +1,24 @@
 package com.rishirajput.weather.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.rishirajput.weather.data.api.WeatherApiService
-import com.rishirajput.weather.data.api.provideRetrofit
 import com.rishirajput.weather.data.repository.RetrofitWeatherRepository
 import com.rishirajput.weather.domain.repository.WeatherRepository
 import com.rishirajput.weather.presentation.viewmodel.WeatherViewModel
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+
+fun provideRetrofit(): Retrofit {
+    val contentType = "application/json".toMediaType()
+    val json = Json { ignoreUnknownKeys = true }
+    return Retrofit.Builder()
+        .baseUrl("https://api.weatherapi.com/v1/")
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .build()
+}
 
 val appModule = module {
     single { provideRetrofit().create(WeatherApiService::class.java) }
