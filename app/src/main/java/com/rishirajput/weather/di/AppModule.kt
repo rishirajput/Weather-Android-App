b47b5da637ac95b713a15e7df8da1ae2eef2d924
rishirajput <rishi_raj@live.com>
2024-12-21 11:10:01 +0530

    refactor AppModule.kt

--- app/src/main/java/com/rishirajput/weather/di/AppModule.kt
+++ app/src/main/java/com/rishirajput/weather/di/AppModule.kt
@@ -2,13 +2,13 @@ package com.rishirajput.weather.di
 
 import com.rishirajput.weather.data.api.WeatherApiService
 import com.rishirajput.weather.data.api.provideRetrofit
-import com.rishirajput.weather.data.repository.WeatherRepository
+import com.rishirajput.weather.data.repository.RetrofitWeatherRepository
 import com.rishirajput.weather.ui.viewmodel.WeatherViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { provideRetrofit().create(WeatherApiService::class.java) }
-    single { WeatherRepository(get()) }
+    single { RetrofitWeatherRepository(get()) }
     viewModel { WeatherViewModel(get()) }
 }
