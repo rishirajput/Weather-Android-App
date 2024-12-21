e61ddcdecd35997bf0799b534b3147cd3cf3567c
rishirajput <rishi_raj@live.com>
2024-12-21 11:34:05 +0530

    update AppModule.kt to provide RetrofitWeatherRepository whenever WeatherRepository dependency is required.

--- app/src/main/java/com/rishirajput/weather/di/AppModule.kt
+++ app/src/main/java/com/rishirajput/weather/di/AppModule.kt
@@ -3,12 +3,13 @@ package com.rishirajput.weather.di
 import com.rishirajput.weather.data.api.WeatherApiService
 import com.rishirajput.weather.data.api.provideRetrofit
 import com.rishirajput.weather.data.repository.RetrofitWeatherRepository
+import com.rishirajput.weather.domain.repository.WeatherRepository
 import com.rishirajput.weather.presentation.viewmodel.WeatherViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { provideRetrofit().create(WeatherApiService::class.java) }
-    single { RetrofitWeatherRepository(get()) }
+    single<WeatherRepository> { RetrofitWeatherRepository(get()) }
     viewModel { WeatherViewModel(get()) }
 }
