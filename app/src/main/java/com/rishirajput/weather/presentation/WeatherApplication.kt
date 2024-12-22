994f7a17fc5351553c9990f9599c5d624a3707de
rishirajput <rishi_raj@live.com>
2024-12-23 00:23:05 +0530

    document WeatherApplication.kt

--- app/src/main/java/com/rishirajput/weather/presentation/WeatherApplication.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/WeatherApplication.kt
@@ -6,6 +6,9 @@ import com.rishirajput.weather.presentation.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+/**
+ * Application class for initializing Koin dependency injection.
+ */
 class WeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
