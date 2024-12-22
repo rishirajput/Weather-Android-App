c754ecee4c3ddac8652b8f844acc681743194f17
rishirajput <rishi_raj@live.com>
2024-12-23 01:06:37 +0530

    document WeatherResponse.kt

--- domain/src/main/java/com/rishirajput/domain/model/WeatherResponse.kt
+++ domain/src/main/java/com/rishirajput/domain/model/WeatherResponse.kt
@@ -2,6 +2,11 @@ package com.rishirajput.domain.model
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Data class representing the weather response.
+ *
+ * @param current The current weather details.
+ */
 @Serializable
 data class WeatherResponse(
     val current: CurrentWeather
