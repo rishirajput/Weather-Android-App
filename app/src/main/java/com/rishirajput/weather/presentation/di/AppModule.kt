b1f182780653823b76ba2075442138738139fd29
rishirajput <rishi_raj@live.com>
2024-12-22 21:47:35 +0530

    document AppModule.kt

--- app/src/main/java/com/rishirajput/weather/presentation/di/AppModule.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/di/AppModule.kt
@@ -4,7 +4,8 @@ import com.rishirajput.weather.presentation.ui.viewmodel.WeatherViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+// Define the app module for dependency injection
 val appModule = module {
+    // Provide the WeatherViewModel
     viewModel { WeatherViewModel(get(), get(), get(), get()) }
 }
-
