92faa80fa7ec23056472561b90487bc0bb74cffb
rishirajput <rishi_raj@live.com>
2024-12-22 11:15:14 +0530

    segregate functions for SRP

--- app/src/main/java/com/rishirajput/weather/data/repository/RetrofitWeatherRepository.kt
+++ app/src/main/java/com/rishirajput/weather/data/repository/RetrofitWeatherRepository.kt
@@ -12,51 +12,44 @@ import retrofit2.HttpException
 import retrofit2.Response
 
 class RetrofitWeatherRepository(private val apiService: WeatherApiService) : WeatherRepository {
-    override suspend fun getWeatherData(query: String): List<WeatherData> {
-        Log.d("RetrofitWeatherRepository", "Searching for location: $query")
-        val locationResponse: Response<List<LocationResponse>> = apiService.searchLocation(BuildConfig.WEATHER_API_KEY, query)
-        Log.d("RetrofitWeatherRepository", "Location response: ${locationResponse.raw()}")
 
+    override suspend fun getWeatherData(query: String): List<WeatherData> {
+        val locations = getLocations(query)
+        return locations.map { getWeatherDataForLocation(it.name) }
+    }
+
+    override suspend fun getLocations(query: String): List<LocationResponse> {
+        val locationResponse: Response<List<LocationResponse>> = apiService.searchLocation(BuildConfig.WEATHER_API_KEY, query)
         if (!locationResponse.isSuccessful || locationResponse.body().isNullOrEmpty()) {
             Log.e("RetrofitWeatherRepository", "No matching location found for query: $query")
             return emptyList()
         }
+        return locationResponse.body()!!
+    }
 
-        val locations = locationResponse.body()!!
-        val weatherDataList = mutableListOf<WeatherData>()
-
-        for (location in locations) {
-            Log.d("RetrofitWeatherRepository", "Fetching weather data for location: ${location.name}")
-            try {
-                val weatherResponse: Response<WeatherResponse> = apiService.getCurrentWeather(BuildConfig.WEATHER_API_KEY, location.name)
-                Log.d("RetrofitWeatherRepository", "Weather response: ${weatherResponse.raw()}")
-
-                if (weatherResponse.isSuccessful) {
-                    val current = weatherResponse.body()?.current
-                    Log.d("RetrofitWeatherRepository", "Received weather data: $current")
-                    weatherDataList.add(
-                        WeatherData(
-                            locationName = location.name,
-                            temperature = current?.temp_c ?: 0.0,
-                            condition = current?.condition?.text ?: "Unknown",
-                            icon = current?.condition?.icon ?: "",
-                            humidity = current?.humidity ?: 0,
-                            uvIndex = current?.uv ?: 0.0,
-                            feelsLike = current?.feelslike_c ?: 0.0
-                        )
-                    )
-                } else {
-                    val errorBody: ResponseBody? = weatherResponse.errorBody()
-                    Log.e("RetrofitWeatherRepository", "HTTP error: ${weatherResponse.code()} ${weatherResponse.message()}")
-                    Log.e("RetrofitWeatherRepository", "Error body: ${errorBody?.string()}")
-                }
-            } catch (e: HttpException) {
-                Log.e("RetrofitWeatherRepository", "HTTP error: ${e.code()} ${e.message()}")
-            } catch (e: Exception) {
-                Log.e("RetrofitWeatherRepository", "Error fetching weather data", e)
+    override suspend fun getWeatherDataForLocation(locationName: String): WeatherData {
+        var weatherData = WeatherData("", 0.0, "", "", 0, 0.0, 0.0)
+        try {
+            val weatherResponse: Response<WeatherResponse> = apiService.getCurrentWeather(BuildConfig.WEATHER_API_KEY, locationName)
+            if (weatherResponse.isSuccessful) {
+                val current = weatherResponse.body()?.current
+                weatherData = weatherData.copy(
+                        locationName = locationName,
+                        temperature = current?.temp_c ?: 0.0,
+                        condition = current?.condition?.text ?: "Unknown",
+                        icon = current?.condition?.icon ?: "",
+                        humidity = current?.humidity ?: 0,
+                        uvIndex = current?.uv ?: 0.0,
+                        feelsLike = current?.feelslike_c ?: 0.0
+                )
+            } else {
+                Log.e("RetrofitWeatherRepository", "HTTP error: ${weatherResponse.code()} ${weatherResponse.message()}")
             }
+        } catch (e: HttpException) {
+            Log.e("RetrofitWeatherRepository", "HTTP error: ${e.code()} ${e.message()}")
+        } catch (e: Exception) {
+            Log.e("RetrofitWeatherRepository", "Error fetching weather data", e)
         }
-
-        return weatherDataList
+        return weatherData
     }
 }
--- app/src/main/java/com/rishirajput/weather/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/rishirajput/weather/domain/repository/WeatherRepository.kt
@@ -1,7 +1,10 @@
 package com.rishirajput.weather.domain.repository
 
+import com.rishirajput.weather.data.api.LocationResponse
 import com.rishirajput.weather.domain.model.WeatherData
 
 interface WeatherRepository {
     suspend fun getWeatherData(query: String): List<WeatherData>
+    suspend fun getLocations(query: String): List<LocationResponse>
+    suspend fun getWeatherDataForLocation(locationName: String): WeatherData
 }
