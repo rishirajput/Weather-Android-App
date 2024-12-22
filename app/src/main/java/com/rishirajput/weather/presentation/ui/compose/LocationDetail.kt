731725a425bee2a384b1088a32219e04dd7a90e5
rishirajput <rishi_raj@live.com>
2024-12-22 14:37:09 +0530

    optimize imports in LocationDetail.kt

--- app/src/main/java/com/rishirajput/weather/presentation/ui/compose/LocationDetail.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/compose/LocationDetail.kt
@@ -1,24 +1,16 @@
 package com.rishirajput.weather.presentation.ui.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rishirajput.domain.model.WeatherData
-import com.rishirajput.weather.R
-import com.rishirajput.weather.presentation.ui.theme.textStyleTemperatureLarge
 
 @Composable
 fun LocationDetail(weatherData: WeatherData) {
