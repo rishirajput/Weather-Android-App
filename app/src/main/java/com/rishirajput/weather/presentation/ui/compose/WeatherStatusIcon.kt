b51875c185a962b7544175969a468790c99b357b
rishirajput <rishi_raj@live.com>
2024-12-23 00:00:09 +0530

    document and create preview for WeatherStatusIcon.kt

--- app/src/main/java/com/rishirajput/weather/presentation/ui/compose/WeatherStatusIcon.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/compose/WeatherStatusIcon.kt
@@ -6,12 +6,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.rishirajput.weather.presentation.ui.utils.updateImageSizeInUrl
 
+/**
+ * Composable function to display the weather status icon.
+ *
+ * @param iconUrl The URL of the weather icon.
+ * @param modifier The modifier to be applied to the composable.
+ */
 @Composable
-fun WeatherStatusIcon(iconUrl: String, modifier: Modifier) {
+fun WeatherStatusIcon(iconUrl: String, modifier: Modifier = Modifier) {
     val painter = rememberAsyncImagePainter(model = updateImageSizeInUrl("https:$iconUrl", 128))
     Image(
         painter = painter,
@@ -21,4 +28,13 @@ fun WeatherStatusIcon(iconUrl: String, modifier: Modifier) {
             .height(128.dp)
             .width(128.dp)
     )
+}
+
+/**
+ * Preview function for WeatherStatusIcon composable.
+ */
+@Preview(showBackground = true)
+@Composable
+fun PreviewWeatherStatusIcon() {
+    WeatherStatusIcon(iconUrl = "//cdn.weatherapi.com/weather/64x64/night/176.png")
 }
