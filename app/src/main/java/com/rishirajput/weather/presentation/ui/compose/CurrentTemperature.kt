c329b7d608ac80288abdedb11b438f08c82fa492
rishirajput <rishi_raj@live.com>
2024-12-24 02:23:32 +0530

    create preview for degree icon

--- app/src/main/java/com/rishirajput/weather/presentation/ui/compose/CurrentTemperature.kt
+++ app/src/main/java/com/rishirajput/weather/presentation/ui/compose/CurrentTemperature.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rishirajput.weather.R
 import com.rishirajput.weather.presentation.ui.theme.textStyleTemperatureLarge
@@ -39,8 +41,20 @@ fun CurrentTemperature(
         Spacer(modifier = Modifier.width(11.dp))
         Image(
             painter = painterResource(id = R.drawable.degree_icon),
-            contentDescription = "Degree Icon",
-            modifier = Modifier.align(Alignment.Top).offset(y = 16.dp)
+            contentDescription = stringResource(R.string.degree_icon),
+            modifier = Modifier
+                .align(Alignment.Top)
+                .offset(y = 16.dp)
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCurrentTemperature() {
+    CurrentTemperature(
+        temperature = 25.0,
+        modifier = Modifier,
+        textStyle = textStyleTemperatureLarge
+    )
 }
