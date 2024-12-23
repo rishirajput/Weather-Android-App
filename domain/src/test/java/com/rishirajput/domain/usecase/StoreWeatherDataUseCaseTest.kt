cd7ffcb40a0df0b1faa25951a544d92976c290ac
rishirajput <rishi_raj@live.com>
2024-12-23 11:00:28 +0530

    write unit tests StoreWeatherDataUseCaseTest.kt

--- domain/src/test/java/com/rishirajput/domain/usecase/StoreWeatherDataUseCaseTest.kt
+++ domain/src/test/java/com/rishirajput/domain/usecase/StoreWeatherDataUseCaseTest.kt
@@ -2,9 +2,11 @@ package com.rishirajput.domain.usecase
 
 import com.rishirajput.domain.model.WeatherData
 import com.rishirajput.domain.repository.LocalStorageRepository
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertThrows
 import org.junit.Test
 
 class StoreWeatherDataUseCaseTest {
@@ -13,7 +15,7 @@ class StoreWeatherDataUseCaseTest {
     private val storeWeatherDataUseCase = StoreWeatherDataUseCase(localStorageRepository)
 
     @Test
-    fun `invoke should call storeWeatherData on repository`() = runBlocking {
+    fun `invoke should call storeWeatherData on repository`() = runBlocking<Unit> {
         // Given
         val weatherData = WeatherData(
             locationName = "Hyderabad",
@@ -23,6 +25,48 @@ class StoreWeatherDataUseCaseTest {
             humidity = 76,
             uvIndex = 4.0,
             feelsLike = 30.4
+        )
+
+        // When
+        storeWeatherDataUseCase(weatherData)
+
+        // Then
+        coVerify { localStorageRepository.storeWeatherData(weatherData) }
+    }
+
+    @Test
+    fun `invoke should handle repository exception`() = runBlocking<Unit> {
+        // Given
+        val weatherData = WeatherData(
+            locationName = "Hyderabad",
+            temperature = 31.0,
+            condition = "Patchy rain nearby",
+            icon = "//cdn.weatherapi.com/weather/64x64/night/176.png",
+            humidity = 76,
+            uvIndex = 4.0,
+            feelsLike = 30.4
+        )
+        coEvery { localStorageRepository.storeWeatherData(any()) } throws RuntimeException("Repository error")
+
+        // When & Then
+        assertThrows(RuntimeException::class.java) {
+            runBlocking {
+                storeWeatherDataUseCase(weatherData)
+            }
+        }
+    }
+
+    @Test
+    fun `invoke should handle different weather data`() = runBlocking<Unit> {
+        // Given
+        val weatherData = WeatherData(
+            locationName = "New York",
+            temperature = 22.0,
+            condition = "Sunny",
+            icon = "//cdn.weatherapi.com/weather/64x64/day/113.png",
+            humidity = 50,
+            uvIndex = 5.0,
+            feelsLike = 21.0
         )
 
         // When
