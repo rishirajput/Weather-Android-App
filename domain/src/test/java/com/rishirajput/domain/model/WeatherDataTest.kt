bfde882c27f4db79486f5a3323a2805548b38dee
rishirajput <rishi_raj@live.com>
2024-12-23 11:11:39 +0530

    write unit tests WeatherDataTest.kt

---
+++ domain/src/test/java/com/rishirajput/domain/model/WeatherDataTest.kt
@@ -0,0 +1,52 @@
+package com.rishirajput.domain.model
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class WeatherDataTest {
+
+    @Test
+    fun `serialize WeatherData to JSON`() {
+        // Given
+        val weatherData = WeatherData(
+            locationName = "Hyderabad",
+            temperature = 31.0,
+            condition = "Patchy rain nearby",
+            icon = "//cdn.weatherapi.com/weather/64x64/night/176.png",
+            humidity = 76,
+            uvIndex = 4.0,
+            feelsLike = 30.4
+        )
+
+        // When
+        val json = Json.encodeToString(weatherData)
+
+        // Then
+        val expectedJson = """{"locationName":"Hyderabad","temperature":31.0,"condition":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png","humidity":76,"uvIndex":4.0,"feelsLike":30.4}"""
+        assertEquals(expectedJson, json)
+    }
+
+    @Test
+    fun `deserialize JSON to WeatherData`() {
+        // Given
+        val json = """{"locationName":"Hyderabad","temperature":31.0,"condition":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png","humidity":76,"uvIndex":4.0,"feelsLike":30.4}"""
+
+        // When
+        val weatherData = Json.decodeFromString<WeatherData>(json)
+
+        // Then
+        val expectedWeatherData = WeatherData(
+            locationName = "Hyderabad",
+            temperature = 31.0,
+            condition = "Patchy rain nearby",
+            icon = "//cdn.weatherapi.com/weather/64x64/night/176.png",
+            humidity = 76,
+            uvIndex = 4.0,
+            feelsLike = 30.4
+        )
+        assertEquals(expectedWeatherData, weatherData)
+    }
+}
--- domain/src/test/java/com/rishirajput/domain/model/WeatherResponseTest.kt
+++ domain/src/test/java/com/rishirajput/domain/model/WeatherResponseTest.kt
@@ -1,6 +1,5 @@
 package com.rishirajput.domain.model
 
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.Assert.assertEquals
@@ -13,7 +12,10 @@ class WeatherResponseTest {
         // Given
         val currentWeather = CurrentWeather(
             tempC = 31.0,
-            condition = Condition("Patchy rain nearby", "//cdn.weatherapi.com/weather/64x64/night/176.png"),
+            condition = Condition(
+                "Patchy rain nearby",
+                "//cdn.weatherapi.com/weather/64x64/night/176.png"
+            ),
             humidity = 76,
             uv = 4.0,
             feelsLikeC = 30.4
@@ -24,14 +26,16 @@ class WeatherResponseTest {
         val json = Json.encodeToString(weatherResponse)
 
         // Then
-        val expectedJson = """{"current":{"temp_c":31.0,"condition":{"text":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png"},"humidity":76,"uv":4.0,"feelslike_c":30.4}}"""
+        val expectedJson =
+            """{"current":{"temp_c":31.0,"condition":{"text":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png"},"humidity":76,"uv":4.0,"feelslike_c":30.4}}"""
         assertEquals(expectedJson, json)
     }
 
     @Test
     fun `deserialize JSON to WeatherResponse`() {
         // Given
-        val json = """{"current":{"temp_c":31.0,"condition":{"text":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png"},"humidity":76,"uv":4.0,"feelslike_c":30.4}}"""
+        val json =
+            """{"current":{"temp_c":31.0,"condition":{"text":"Patchy rain nearby","icon":"//cdn.weatherapi.com/weather/64x64/night/176.png"},"humidity":76,"uv":4.0,"feelslike_c":30.4}}"""
 
         // When
         val weatherResponse = Json.decodeFromString<WeatherResponse>(json)
@@ -39,7 +43,10 @@ class WeatherResponseTest {
         // Then
         val expectedCurrentWeather = CurrentWeather(
             tempC = 31.0,
-            condition = Condition("Patchy rain nearby", "//cdn.weatherapi.com/weather/64x64/night/176.png"),
+            condition = Condition(
+                "Patchy rain nearby",
+                "//cdn.weatherapi.com/weather/64x64/night/176.png"
+            ),
             humidity = 76,
             uv = 4.0,
             feelsLikeC = 30.4
